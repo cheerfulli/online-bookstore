32b1c8e2bcd22243c5f3165462ea209396952b0c
cheerfulli <1511240898@qq.com>
2026-02-12 00:02:47 +0800

    [部署]给前后端添加docker配置文件

--- code/springboot/src/main/java/com/lifeng/springboot/mapper/RefundOrderMapper.java
+++ code/springboot/src/main/java/com/lifeng/springboot/mapper/RefundOrderMapper.java
@@ -3,9 +3,6 @@ package com.lifeng.springboot.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lifeng.springboot.entity.RefundOrder;
 
-/**
- * @Author: Xia
- * @Date: 2024/3/2  16:01
- */
+
 public interface RefundOrderMapper extends BaseMapper<RefundOrder> {
 }
--- code/springboot/src/main/java/com/lifeng/springboot/mapper/UserAdviceMapper.java
+++ code/springboot/src/main/java/com/lifeng/springboot/mapper/UserAdviceMapper.java
@@ -3,9 +3,6 @@ package com.lifeng.springboot.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lifeng.springboot.entity.UserAdvice;
 
-/**
- * @Author: Xia
- * @Date: 2023/11/17  16:35
- */
+
 public interface UserAdviceMapper extends BaseMapper<UserAdvice> {
 }
